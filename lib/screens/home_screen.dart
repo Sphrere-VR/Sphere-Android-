7bc830657aa6f699413d3782f2e354bad2d9d40b
edwards698 <edwardphirijr698@gmail.com>
2025-02-23 17:20:06 +0200

    Update homeUI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,22 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sphere App',
       theme: ThemeData(
-        // Set a default font to see if it applies
-        textTheme: GoogleFonts.creepsterTextTheme(),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        textTheme: GoogleFonts
+            .luckiestGuyTextTheme(), // Set the Luckiest Guy font globally
       ),
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
@@ -27,21 +23,172 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Creepster Font Test",
-          style: GoogleFonts.creepster(), // Apply the font here
-        ),
+      body: Stack(
+        children: [
+          // Background Gradient
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 246, 185, 59), // Gold color
+                  Color.fromRGBO(105, 240, 174, 1)
+                      .withOpacity(0.5), // Light green with opacity
+                ],
+              ),
+            ),
+          ),
+
+          // Main Content
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Game Image
+              Container(
+                height: 350,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/cover.png'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+
+              // Card Container with Border
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.9),
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2, // Border thickness
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "It's time to",
+                      style: TextStyle(
+                        fontFamily:
+                            'lucky', // The family name you defined in pubspec.yaml
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Save the Galaxy !",
+                      style: TextStyle(
+                        fontFamily:
+                            'lucky', // The family name you defined in pubspec.yaml
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 246, 185, 59),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Join millions of players worldwide to win a race and compete in Mario Kart races.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily:
+                            'lucky', // The family name you defined in pubspec.yaml
+                        fontSize: 15,
+                        color: Colors.black54,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // Play Button Inside Card
+                    GestureDetector(
+                      onTap: () {
+                        // Add your navigation action here
+                      },
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                        decoration: BoxDecoration(
+                          color: Colors.greenAccent,
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(
+                            color:
+                                Color.fromARGB(255, 12, 12, 12), // Border color
+                            width: 2, // Border thickness
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.greenAccent.withOpacity(0.5),
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              offset: Offset(0, 5),
+                            ),
+                          ],
+                        ),
+                        child: Text(
+                          "PLAY NOW",
+                          style: TextStyle(
+                            fontFamily:
+                                'lucky', // The family name you defined in pubspec.yaml
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 246, 185, 59),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+// "Made and Designed by EPJ" at Bottom with Grey Opacity Text
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20), // Adjust bottom spacing
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Made and Designed by ",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black.withOpacity(0.5), // Grey opacity
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print('EPJ link tapped');
+                    },
+                    child: Text(
+                      "Edward",
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontSize: 12,
+                        color: Colors.blue
+                            .withOpacity(0.5), // Grey opacity for the link
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-          child: Text(
-        "Hi Cecilia",
-        style: TextStyle(
-          fontFamily: 'lucky', // The family name you defined in pubspec.yaml
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      )),
     );
   }
 }
