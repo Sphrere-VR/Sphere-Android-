72aab6b965cff7ea1d73a98e0730f8fd56fb5bd0
edwards698 <edwardphirijr698@gmail.com>
2025-02-23 14:49:56 +0200

    Update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Sphere App',
+      theme: ThemeData(
+        // Set a default font to see if it applies
+        textTheme: GoogleFonts.creepsterTextTheme(),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
       home: HomeScreen(),
     );
   }
@@ -19,64 +27,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Colors.greenAccent, Colors.green],
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                '/assets/images/tools.png', // Replace with your image
-                height: 200,
-              ),
-              SizedBox(height: 20),
-              Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.8),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      "It's time to",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                    ),
-                    Text(
-                      "Race!",
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Join millions of players worldwide to win a race and compete in Mario Kart races.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16, color: Colors.black54),
-                    ),
-                    SizedBox(height: 20),
-                    CircleAvatar(
-                      radius: 25,
-                      backgroundColor: Colors.green,
-                      child: Icon(Icons.arrow_forward, color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+      appBar: AppBar(
+        title: Text(
+          "Creepster Font Test",
+          style: GoogleFonts.creepster(), // Apply the font here
         ),
       ),
+      body: Center(
+          child: Text(
+        "Hi Cecilia",
+        style: TextStyle(
+          fontFamily: 'lucky', // The family name you defined in pubspec.yaml
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      )),
     );
   }
 }
